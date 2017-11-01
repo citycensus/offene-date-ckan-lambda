efbbacb33229683a2a8c98d4105f7168d6cb9c35
milafrerichs <mila.frerichs@gmail.com>
2017-11-01 10:02:00 +0100

    Update city_types and refactor methods

--- orgs.py
+++ orgs.py
@@ -1,3 +1,4 @@
+# -*- coding: latin-1 -*-
 import sys, os, json
 
 here = os.path.dirname(os.path.realpath(__file__))
@@ -11,7 +12,7 @@ import simplejson as json
 import boto3
 
 ua = 'ckanapiexample/1.0 (+http://example.com/my/website)'
-stadt_types = ('Stadt', 'Landeshauptstadt', 'Freie und Hansestadt')
+stadt_types = ('Stadt', 'Landeshauptstadt', 'Freie und Hansestadt', 'Hansestadt', u'Universitätsstadt', 'Verbandsgemeinde', 'Kreisstadt') #Landkreis ?
 od = RemoteCKAN('https://offenedaten.de')
 
 def upload_file_to_s3(bucket_file, local_file):
@@ -20,7 +21,6 @@ def upload_file_to_s3(bucket_file, local_file):
 
 def get_org_detail(org_id):
     od_org = od.action.organization_show(id=org_id)
-    is_city = False
     org = {
             'name': od_org['display_name'],
             'created_at': od_org['created'],
@@ -29,7 +29,8 @@ def get_org_detail(org_id):
             'latitude': 0,
             'longitude': 0,
             'contact_person': '',
-            'contact_email': ''
+            'contact_email': '',
+            'city_type': ''
         }
     for extra in od_org['extras']:
         if extra['key'] == 'latitude':
@@ -43,15 +44,21 @@ def get_org_detail(org_id):
         elif extra['key'] == 'open_data_portal':
             org['portal'] = extra['value']
         elif extra['key'] == 'city_type':
-            if extra['value'] in stadt_types:
-                is_city = True
-    if is_city:
+            org['city_type'] = extra['value']
+    return(org)
+
+def collect_org(org_id):
+    org = get_org_detail(org_id)
+    if org['city_type'] in stadt_types:
         return(org)
 
-def organisations(event, context):
+def get_all_orgs():
     orgs = od.action.organization_list()
-    rows = [get_org_detail(org) for org in orgs]
-    table = agate.Table.from_object(filter(None, rows))
+    rows = [collect_org(org) for org in orgs]
+    return agate.Table.from_object(filter(None, rows))
+
+def organisations(event, context):
+    table = get_all_orgs()
     table.to_csv('/tmp/open_data_germany.csv')
     upload_file_to_s3('open_data_cities.csv','/tmp/open_data_germany.csv')
     aggregates = table.aggregate([
@@ -72,4 +79,3 @@ def organisations(event, context):
     }
 
     return response
-organisations({},{})
