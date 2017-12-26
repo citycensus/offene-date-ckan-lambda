bedcd1f8222e0ea9e211fe4e5bf016baaa4ea8bc
milafrerichs <mila.frerichs@gmail.com>
2017-12-26 08:49:01 +0100

    Split up collecting and transformation
    collecting from the API into separate class
    organisation transformation as well
    save all the stats into an organisation for easier saving/writing to
    file

---
+++ tests/offene_daten_api_test.py
@@ -0,0 +1,46 @@
+import unittest
+import httpretty
+
+from analysis.offene_daten_api import OffeneDatenAPI
+
+class TestOffeneDatenAPI(unittest.TestCase):
+    def setUp(self):
+        self.od = OffeneDatenAPI()
+    def tearDown(self):
+        self.od = None
+
+    @httpretty.activate
+    def test_get_all_cities(self):
+        httpretty.register_uri(httpretty.POST, "https://offenedaten.de/api/action/organization_list",
+                body='{"success": true, "result": []}',
+                content_type="application/json")
+        assert self.od.get_all_orgs() == []
+
+    @httpretty.activate
+    def get_all_orgs_single_test(self):
+        httpretty.register_uri(httpretty.POST, "https://offenedaten.de/api/action/organization_list",
+                body='{"success": true, "result": ["berlin"]}',
+                content_type="application/json")
+        assert self.od.get_all_orgs() == ['berlin']
+
+    @httpretty.activate
+    def get_org_data_test(self):
+        httpretty.register_uri(httpretty.POST, "https://offenedaten.de/api/action/organization_show?id=berlin&include_datasets=False",
+                body='{"success": true, "result": { "name": "berlin", "extras": [{ "key": "city_type", "value": "Stadt"}]}}',
+                content_type="application/json")
+        assert self.od.get_org_data('berlin') == { "name": "berlin", "extras": [{ "key": "city_type", "value": "Stadt"}]}
+
+    @httpretty.activate
+    def get_org_data_include_datasets_test(self):
+        httpretty.register_uri(httpretty.POST, "https://offenedaten.de/api/action/organization_show?id=berlin&include_datasets=True",
+                body='{"success": true, "result": { "name": "berlin", "packages": [], "extras": [{ "key": "city_type", "value": "Stadt"}]}}',
+                content_type="application/json")
+        assert self.od.get_org_data('berlin') == { "name": "berlin", "packages": [], "extras": [{ "key": "city_type", "value": "Stadt"}]}
+
+    @httpretty.activate
+    def get_package_data_test(self):
+        httpretty.register_uri(httpretty.POST, "https://offenedaten.de/api/action/package_show?id=berlin-data",
+                body='{"success": true, "result": { "name": "berlin", "resources": []}}',
+                content_type="application/json")
+        assert self.od.get_package_data('berlin') == { "name": "berlin", "resources": []}
+
