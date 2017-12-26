a12da4740a0b12a2165a98e5e802944123ad044a
milafrerichs <mila.frerichs@gmail.com>
2017-12-26 08:43:01 +0100

    Score each package individually and add a group score
    each package gets scored individually for openness and machine
    readability
    do not use `isopen` but rather the license id itself to score for open
    license

--- analysis/utils.py
+++ analysis/utils.py
@@ -9,6 +9,20 @@ import decimal
 number = agate.Number()
 text = agate.Text()
 OD_BUCKET = 'open-data-germany-orgs'
+GROUPS = ("bevoelkerung",
+    "bildung-und-wissenschaft",
+    "geographie-geologie-und-geobasisdaten",
+    "gesetze-und-justiz",
+    "gesundheit",
+    "infrastruktur-bauen-und-wohnen",
+    "kultur-freizeit-sport-tourismus",
+    "oeffentliche-verwaltung-haushalt-und-steuern",
+    "politik-und-wahlen",
+    "soziales",
+    "transport-und-verkehr",
+    "umwelt-und-klima",
+    "verbraucherschutz",
+    "wirtschaft-und-arbeit")
 ORG_COLUMN_TYPE = {
         'id': text,
         'name': text,
@@ -27,11 +41,17 @@ ORG_COLUMN_TYPE = {
         'days_since_start': number,
         'days_between_start_and_last_update': number,
         'category_count': number,
+        'category_score': number,
         'category_variance': number,
         'open_license_and_format_count': number,
+        'dataset_score': number,
         }
 
 OPEN_FORMATS = ['csv', 'xml', 'json', 'geojson', 'gml', 'rss','txt', 'tsv', 'tiff']
+MACHINE_READABLE_FORMATS = ['csv', 'xml', 'json', 'geojson', 'gml', 'rss','txt', 'tsv', 'tiff', 'xlsx', 'xls']
+OPEN_LICENSES = ("cc-by", "odc-by", "cc-by 3.0", "dl-de-by-2.0", "dl-de/by-2-0", "cc-by-sa 3.0", "other-open", "cc0-1.0", "cc-zero", "dl-de-zero-2.0", "andere offene lizenzen", "cc by 3.0 de", "dl-de-by-1.0", "dl-de-by 1.0", "gfdl", "odbl", "cc-by-sa", "https://creativecommons.org/licenses/by/3.0/de/", "https://www.govdata.de/dl-de/by-2-0", "cc-by-3.0", "odc-odbl", "cc-by-4.0", "https://www.govdata.de/dl-de/zero-2-0")
+
+
 
 def upload_file_to_s3(bucket_file, local_file):
     s3 = boto3.resource('s3')
@@ -47,6 +67,7 @@ def transform_file(s3_file):
                 'Key': s3_file['Key']
             }
         )
+        print('city:', s3_file['Key'])
         return agate.Table.from_url(url,column_types=ORG_COLUMN_TYPE)
     return None
 
@@ -68,15 +89,13 @@ def collect_all_cities():
 """
 def overall_rank(row):
     return(
-        decimal.Decimal(0.3)*row['last_update_rank']+
-        decimal.Decimal(0.2)*row['open_datasets_rank']+
-        decimal.Decimal(0.1)*row['open_formats_rank']+
-        decimal.Decimal(0.1)*row['category_variance_rank']+
-        decimal.Decimal(0.1)*row['start_rank'] +
-        decimal.Decimal(0.05)*row['dataset_rank']+
-        decimal.Decimal(0.05)*row['formats_rank']+
-        decimal.Decimal(0.05)*row['category_rank']+
-        decimal.Decimal(0.05)*row['update_start_rank']
+        decimal.Decimal(0.4)*row['dataset_score_rank_std']+
+        decimal.Decimal(0.2)*row['category_score_rank_std']+
+        decimal.Decimal(0.15)*row['category_variance_rank_std']+
+        decimal.Decimal(0.1)*row['dataset_rank_std']+
+        decimal.Decimal(0.05)*row['formats_rank_std']+
+        decimal.Decimal(0.05)*row['last_update_rank_std']+
+        decimal.Decimal(0.05)*row['category_rank_std']
     )
 
 def openness_score(row):
@@ -95,21 +114,51 @@ def compute_ranks(table):
         ('category_variance_rank', agate.Rank('category_variance')),
         ('update_start_rank', agate.Rank('days_between_start_and_last_update')),
         ('start_rank', agate.Rank('days_since_start', reverse=True)),
+        ('openess_score', agate.Formula(number, openness_score)),
+        ('dataset_score_rank', agate.Rank('dataset_score', reverse=True)),
+        ('category_score_rank', agate.Rank('category_score', reverse=True)),
     ])
     table = table.compute([
-        ('overall_rank_data', agate.Formula(number, overall_rank)),
-        ('openess_score', agate.Formula(number, openness_score)),
+        ('dataset_score_rank_std', StandadizeScore('dataset_score_rank')),
+        ('dataset_rank_std', StandadizeScore('dataset_rank')),
+        ('category_score_rank_std', StandadizeScore('category_score_rank')),
+        ('category_variance_rank_std', StandadizeScore('category_variance_rank')),
+        ('formats_rank_std', StandadizeScore('formats_rank')),
+        ('last_update_rank_std', StandadizeScore('last_update_rank')),
+        ('category_rank_std', StandadizeScore('category_rank')),
         ])
+    table = table.compute([
+        ('overall_rank_data', agate.Formula(agate.Number(), overall_rank))
+    ])
     table = table.compute([
         ('overall_rank', agate.Rank('overall_rank_data')),
         ])
     return table
 
 def filter_ranks(table):
-    include_columns = ['id','overall_rank', 'dataset_rank', 'formats_rank', 'open_formats_rank', 'last_update_rank', 'open_datasets_rank', 'category_rank', 'category_variance_rank', 'update_start_rank', 'start_rank']
+    include_columns = ['id','overall_rank', 'dataset_rank', 'formats_rank', 'open_formats_rank', 'last_update_rank', 'open_datasets_rank', 'category_rank', 'category_variance_rank', 'update_start_rank', 'start_rank', 'dataset_score_rank']
     return table.select(include_columns)
 
 def has_open_format_in_resources(data):
     resources = [resource["format"] for resource in data["resources"] ]
     return any(f.lower() in OPEN_FORMATS for f in resources)
 
+class StandadizeScore(agate.Computation):
+    def __init__(self, column_name):
+        self._column_name = column_name
+
+        super(StandadizeScore, self).__init__()
+
+    def get_computed_data_type(self, table):
+        return agate.Number()
+
+    def run(self, table):
+        new_column = []
+
+        rank_values = [r[self._column_name] for r in table.rows]
+        max_rank = max(rank_values)
+        for i, row in enumerate(table.rows):
+            value = row[self._column_name]
+            new_column.append(value/max_rank*100)
+
+        return new_column
--- tests/utils_test.py
+++ tests/utils_test.py
@@ -6,7 +6,7 @@ import analysis.utils as utils
 
 class TestUtils(object):
     def test_overall_rank(self):
-        row = { "dataset_rank": 1, "formats_rank": 1, "open_formats_rank": 1, "last_update_rank": 1, "open_datasets_rank": 1, "category_variance_rank": 1, "start_rank": 1, "category_rank": 1, "update_start_rank": 1}
+        row = { "dataset_rank": 1, "formats_rank": 1, "open_formats_rank": 1, "last_update_rank": 1, "open_datasets_rank": 1, "category_variance_rank": 1, "start_rank": 1, "category_rank": 1, "update_start_rank": 1, "dataset_score_rank": 1, "category_score_rank": 1}
         assert utils.overall_rank(row) == decimal.Decimal(1.0)
 
     def test_openness_score_no_l_f(self):
