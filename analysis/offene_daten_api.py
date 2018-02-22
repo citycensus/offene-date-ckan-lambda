df99d675b199bac98f7fc8b3f37db25c72d3ef42
milafrerichs <mila.frerichs@gmail.com>
2018-02-22 07:59:52 +0100

    Use package_search for cities that have more than 1000 datasets

--- analysis/offene_daten_api.py
+++ analysis/offene_daten_api.py
@@ -11,5 +11,15 @@ class OffeneDatenAPI(object):
         return self.od.action.organization_list()
 
     def get_org_data(self, org_id, include_datasets = False):
-        return self.od.action.organization_show(id=org_id, include_datasets=include_datasets)
+        datasets = self.od.action.organization_show(id=org_id, include_datasets=include_datasets)
+        if include_datasets and datasets["package_count"] > 1000:
+            pages = datasets["package_count"] / 1000
+            for page in range(1,pages+1):
+                additional_datasets = self.get_org_packages(org_id, page)
+                datasets["packages"] = datasets["packages"] + additional_datasets['results']
+        return datasets
+
+    def get_org_packages(self, org_id, page):
+        search = "organization:{}".format(org_id)
+        return self.od.action.package_search(fq=search, rows=1000, start=(1000*page))
 
